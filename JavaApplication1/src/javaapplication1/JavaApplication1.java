14d8ca59241d4e9fb600a6f436086a0f7d5e1dbe
Luana <luanap.teixeira@hotmail.com>
2024-03-21 07:57:52 -0300

    Initial commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author hahba
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Olá, Mundo");
+       JOptionPane.showMessageDialog(null, "Olá, mundo");
+    }
+    
+}
---
+++ WebApplication1/build/web/newjsp.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : newjsp
+    Created on : 12 de mar. de 2024, 22:08:28
+    Author     : hahba
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>JSP Page</h1>
+        <div> <%= new java.util.Date()%> </div>
+    </body>
+</html>
